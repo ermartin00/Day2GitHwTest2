d4663ae8b2a7fa3acde2087ad372093d23030714
ermartin00 <140658480+ermartin00@users.noreply.github.com>
2023-07-29 15:18:12 -0400

    Update GitHWTest3.java

--- src/git/hw2/GitHWTest3.java
+++ src/git/hw2/GitHWTest3.java
@@ -4,5 +4,8 @@ public class GitHWTest3 {
 	public static void main(String[] args) {
 		System.out.println("Today weather condition");
 	}
-	
+	public static void getInfo() {
+		System.out.println("this is your personal info");
+		System.out.println("Your name is John");
+	}
 }
