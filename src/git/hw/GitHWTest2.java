216105a6cdd023403b97d401fee3b10ade3f3aab
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-29 15:12:28 -0400

    implemented a getCountry() method

--- src/git/hw/GitHWTest2.java
+++ src/git/hw/GitHWTest2.java
@@ -5,4 +5,8 @@ public class GitHWTest2 {
 	public static void main(String[] args) {
 		System.out.println("demo2");
 	}
+	
+	public static void getCountry(String country) {
+		System.out.println("Country: " + country);
+	}
 }
