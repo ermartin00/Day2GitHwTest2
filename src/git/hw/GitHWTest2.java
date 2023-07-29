0b55be91128db488d1443d6336ef213b25402907
ermartin00 <140658480+ermartin00@users.noreply.github.com>
2023-07-29 15:13:21 -0400

    Update GitHWTest2.java

--- src/git/hw/GitHWTest2.java
+++ src/git/hw/GitHWTest2.java
@@ -9,4 +9,9 @@ public class GitHWTest2 {
 	public static void getCountry(String country) {
 		System.out.println("Country: " + country);
 	}
+
+	public static void getState(String state) {
+		System.out.println("State: " + state);
+	}
+	
 }
