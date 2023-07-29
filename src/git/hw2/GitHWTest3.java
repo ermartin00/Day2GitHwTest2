ba00b7df33b317aa41943f85d336b7b50ce8a655
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-29 15:26:33 -0400

    update teh class

--- src/git/hw2/GitHWTest3.java
+++ src/git/hw2/GitHWTest3.java
@@ -4,8 +4,13 @@ public class GitHWTest3 {
 	public static void main(String[] args) {
 		System.out.println("Today weather condition");
 	}
+	
 	public static void getData() {
 		System.out.println("this is your personal info");
 		System.out.println("Your name is John");
+	}
+	
+	public static void getInfo() {
+		System.out.println("this is your info");
 	}
 }
